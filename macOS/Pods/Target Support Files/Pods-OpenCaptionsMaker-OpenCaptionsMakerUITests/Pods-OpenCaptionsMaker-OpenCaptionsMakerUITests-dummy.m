a437ff996afdc7f5d987862bab188dd71962c3ab
Dylan Klein <dylanklein29@gmail.com>
2020-05-10 16:46:10 +1000

    updated gitignore

--- macOS/Pods/Target Support Files/Pods-OpenCaptionsMaker-OpenCaptionsMakerUITests/Pods-OpenCaptionsMaker-OpenCaptionsMakerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenCaptionsMaker_OpenCaptionsMakerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OpenCaptionsMaker_OpenCaptionsMakerUITests
-@end
--- macOS/Pods/Target Support Files/Pods-OpenCaptionsMaker/Pods-OpenCaptionsMaker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenCaptionsMaker : NSObject
-@end
-@implementation PodsDummy_Pods_OpenCaptionsMaker
-@end
