a90319542ec1b7a5be614737ed795365cb815996
Dylan Klein <dylanklein29@gmail.com>
2020-05-24 17:12:51 +1000

    UI enhancements - issue #4

--- macOS/OpenCaptionsMaker/Supporting Views/CaptionRow.swift
+++ macOS/OpenCaptionsMaker/Supporting Views/CaptionRow.swift
@@ -62,6 +62,7 @@ struct CaptionRow: View {
         case row, text, startTime, endTime
     }
     
+    // Handle mouse clicks
     func setStateOnTap(fromView view: CaptionElement) -> Void {
         switch view {
         case .row:
@@ -83,7 +84,7 @@ struct CaptionRow: View {
         case .startTime:
             switch state.mode {
                 case .play: self.state.mode = .pause
-                case .pause: if isSelected { self.state.mode = .edit }
+                case .pause: if isSelected { self.state.mode = .editStartTime }
                 case .edit: self.state.mode = .editStartTime
                 case .editStartTime: ()
                 case .editEndTime: self.state.mode = .editStartTime
@@ -91,7 +92,7 @@ struct CaptionRow: View {
         case .endTime:
             switch state.mode {
                 case .play: self.state.mode = .pause
-                case .pause: if isSelected { self.state.mode = .edit }
+                case .pause: if isSelected { self.state.mode = .editEndTime }
                 case .edit: self.state.mode = .editEndTime
                 case .editStartTime: self.state.mode = .editEndTime
                 case .editEndTime: ()
@@ -100,101 +101,93 @@ struct CaptionRow: View {
         self.state.selectionIndex = self.captionIndex
     }
     
+    // Draw a box when element is selected
+    struct SelectionBox: View {
+        
+        var body: some View {
+            RoundedRectangle(cornerRadius: 5)
+                .fill(Color.clear)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .strokeBorder(Color.white, lineWidth: 2)
+                )
+        }
+    }
+    
+    // Geometric variables for even spacing
+    // Caption timing values
+    let timeWidth = CGFloat(100.0)
+    let timePadding = CGFloat(20.0)
+    // Caption Text
+    let textWidth = CGFloat(300.0)
+    let textOffset = CGFloat(-40.0)
+    let deltaOffset = CGFloat(6.0)
+    // Scrollbar
+    let scrollbarPadding = CGFloat(20.0)
+    
     var body: some View {
         
         ZStack {
-            
+        
             // Row background
             RoundedRectangle(cornerRadius: 10).fill(rowColor)
                 .frame(height: 40)
                 .onTapGesture { self.setStateOnTap(fromView: .row) }
-            
+                
             // Caption contents
             HStack(alignment: .center) {
                 
-                // Display caption timings
-                VStack {
-                    if (state.mode == .edit || state.mode == .editStartTime || state.mode == .editEndTime) && isSelected {
-                        Stepper(value: captionBinding.startTime, step: -0.1) {
-                            ZStack {
-                                Text(String(format: "%.1f", caption.startTime))
-                                    .onTapGesture { self.setStateOnTap(fromView: .startTime) }
-                                if state.mode == .editStartTime {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .fill(Color.clear)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 5)
-                                                .strokeBorder(Color.white, lineWidth: 2)
-                                    )
+                if isSelected {
+                    // Display caption timings
+                    VStack {
+                        if self.state.mode == .editStartTime {
+                            Stepper(value: captionBinding.startTime, step: -0.1) {
+                                ZStack {
+                                    Text(String(format: "%.1f", caption.startTime))
+                                        .onTapGesture { self.setStateOnTap(fromView: .startTime) }
+                                    SelectionBox()
                                 }
                             }
-                            .padding(.leading)
+                            .padding(.leading, timePadding)
+                        } else {
+                            Text(String(format: "%.1f", caption.startTime))
+                                .onTapGesture { self.setStateOnTap(fromView: .startTime) }
                         }
                         Spacer()
-                        Stepper(value: captionBinding.endTime, step: -0.1) {
-                            ZStack {
-                                Text(String(format: "%.1f", caption.endTime))
-                                    .onTapGesture { self.setStateOnTap(fromView: .endTime) }
-                                if state.mode == .editEndTime {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .fill(Color.clear)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 5)
-                                                .strokeBorder(Color.white, lineWidth: 2)
-                                    )
+                        if self.state.mode == .editEndTime {
+                            Stepper(value: captionBinding.endTime, step: -0.1) {
+                                ZStack {
+                                    Text(String(format: "%.1f", caption.endTime))
+                                        .onTapGesture { self.setStateOnTap(fromView: .endTime) }
+                                    SelectionBox()
                                 }
                             }
-                            .padding(.leading)
+                            .padding(.leading, timePadding)
+                        } else {
+                            Text(String(format: "%.1f", caption.endTime))
+                                .onTapGesture { self.setStateOnTap(fromView: .endTime) }
                         }
-                    } else {
-                        Text(String(format: "%.1f", caption.startTime))
-                            .onTapGesture {
-                                self.setStateOnTap(fromView: .startTime)
-                            }
-                        Spacer()
-                        Text(String(format: "%.1f", caption.endTime))
-                            .onTapGesture {
-                                self.setStateOnTap(fromView: .endTime)
-                            }
                     }
-                }
-                .frame(width: 80.0)
-                
-                Spacer()
-                
-                ZStack {
-                    Text(caption.text)
+                    .frame(width: timeWidth)
+                    Spacer()
+                    // Display caption text
+                    ZStack {
+                        Text(caption.text)
                         .multilineTextAlignment(.center)
                         .lineLimit(2)
-                        .frame(width: 300)
-                        .offset(x: -30)
-                        .onTapGesture {
-                            self.setStateOnTap(fromView: .text)
-                        }
-                    if state.mode == .edit && isSelected {
-                        RoundedRectangle(cornerRadius: 5)
-                            .fill(Color.clear)
-                            .frame(width: 300, height: 20)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .strokeBorder(Color.white, lineWidth: 2)
-                        )
+                        .onTapGesture { self.setStateOnTap(fromView: .text) }
+                        if self.state.mode == .edit { SelectionBox() }
                     }
-                }
-                
-                Spacer()
-                
-                // Display insert plus and minus icons
-                VStack {
-                    
-                    if isSelected {
+                    .offset(x: textOffset + deltaOffset)
+                    .frame(width: textWidth)
+                    Spacer()
+                    VStack {
                         Button(action: {
                             addCaption(beforeIndex: self.captionIndex, atTime: self.caption.startTime)
                         }) {
                             IconView("NSAddTemplate")
                                 .frame(width: 12, height: 12)
                         }
-
                         Button(action: {
                             deleteCaption(atIndex: self.captionIndex)
                         }) {
@@ -203,21 +196,43 @@ struct CaptionRow: View {
                                 .frame(width: 12, height: 12)
                             }
                         }
-                    } else {
-                        Spacer()
                     }
+                    .buttonStyle(buttonStyle)
+                    
+                } else if !isSelected {
+                    // Display caption timings
+                    VStack {
+                        Text(String(format: "%.1f", caption.startTime))
+                            .onTapGesture { self.setStateOnTap(fromView: .startTime) }
+                        Spacer()
+                        Text(String(format: "%.1f", caption.endTime))
+                           .onTapGesture { self.setStateOnTap(fromView: .endTime) }
+                    }
+                    .frame(width: timeWidth)
+                    Spacer()
+                    // Display caption text
+                    Text(caption.text)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(2)
+                        .frame(width: textWidth)
+                        .offset(x: textOffset)
+                        .onTapGesture { self.setStateOnTap(fromView: .text) }
+                    Spacer()
                 }
-                .buttonStyle(buttonStyle)
-                .padding(.trailing)
             }
+            .padding(.trailing, scrollbarPadding)
         }
         .frame(height: 30)
     }
 }
 
+var test = CaptionListState()
+
 struct CaptionRow_Previews: PreviewProvider {
+
     static var previews: some View {
         CaptionRow(caption: userData.captions[0])
             .frame(height: 100)
+            .environmentObject(test)
     }
 }
