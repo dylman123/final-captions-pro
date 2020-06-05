484554d26185822bf26167081e8d0557495f1780
Dylan Klein <dylanklein29@gmail.com>
2020-06-05 18:18:45 +1000

    fixed drag issue of continuity - issue #7

--- macOS/OpenCaptionsMaker/Supporting Views/VideoPane/VisualOverlay.swift
+++ macOS/OpenCaptionsMaker/Supporting Views/VideoPane/VisualOverlay.swift
@@ -17,13 +17,8 @@ struct VisualOverlay: View {
     
     @State var xPos: Float
     @State var yPos: Float
-//    private var offset: CGSize {
-//        let w = CGFloat(self.xPos)
-//        let h = CGFloat(self.yPos)
-//        return CGSize(width: w, height: h)
-//    }
     @State private var offset: CGSize = .zero
-    @State private var translation: CGSize = .zero
+    @State private var position: CGSize = .zero
     //private var xPos: CGFloat = 0.0
     //private var yPos: CGFloat = 200.0
     
@@ -34,26 +29,16 @@ struct VisualOverlay: View {
             Rectangle().fill(Color.blue.opacity(0.001))
             
             Text(caption.text)
-                //.offset(x: CGFloat(xPos), y: CGFloat(yPos))
-                //.offset(x: CGFloat(caption.style.xPos), y: CGFloat(caption.style.yPos))
                 .offset(x: self.offset.width, y: self.offset.height)
                 .gesture(
                     DragGesture()
                         .onChanged { gesture in
-                            self.translation = gesture.translation
-                            //self.xPos = Float(gesture.translation.width)
-                            //self.yPos = Float(gesture.translation.height)
+                            self.offset.width = self.position.width + gesture.translation.width
+                            self.offset.height = self.position.height + gesture.translation.height
                         }
 
                         .onEnded { _ in
-                            self.offset = self.translation
-                            //self.offset = gesture.predictedEndTranslation
-//                            self.app.captions[self.index].style.xPos = Float(self.offset.width)
-//                            self.app.captions[self.index].style.yPos = Float(self.offset.height)
-//                            print("x: ", self.app.captions[self.index].style.xPos, "y: ", self.app.captions[self.index].style.yPos)
-//                            self.offset.width = CGFloat(self.caption.style.xPos)
-//                            self.offset.height = CGFloat(self.caption.style.yPos)
-//                            print("X: ", self.caption.style.xPos, "Y: ", self.caption.style.yPos)
+                            self.position = self.offset
                         }
                 )
         }
