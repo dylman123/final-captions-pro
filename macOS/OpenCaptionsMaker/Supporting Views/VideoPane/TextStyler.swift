0f25df8b479d6b2743a69165cc8f3d9e1da0645c
Dylan Klein <dylanklein29@gmail.com>
2020-06-10 22:27:49 +1000

    aesthetic touch - issue #9

--- macOS/OpenCaptionsMaker/Supporting Views/VideoPane/TextStyler.swift
+++ macOS/OpenCaptionsMaker/Supporting Views/VideoPane/TextStyler.swift
@@ -12,6 +12,9 @@ struct TextStyler: View {
     
     @EnvironmentObject var app: AppState
     
+    // To format the buttons
+    var buttonStyle = BorderlessButtonStyle()
+    
     func updateAlignment(to alignment: TextAlignment) -> Void {
         app.captions[app.selectedIndex].style.alignment = alignment
         publishToVisualOverlay(animate: true)
@@ -24,41 +27,49 @@ struct TextStyler: View {
     var body: some View {
         
         ZStack {
-            RoundedRectangle(cornerRadius: 5)
-            .fill(Color.black)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .strokeBorder(Color.white, lineWidth: 3)
-            )
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.black.opacity(0.8))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .strokeBorder(Color.white, lineWidth: 3)
+                )
             
             HStack {
-                IconView("NSTouchBarTextBoldTemplate")
-                IconView("NSTouchBarTextItalicTemplate")
-                IconView("NSTouchBarTextUnderlineTemplate")
-                
-                Button(action: {
-                    self.updateAlignment(to: .leading)
-                }, label: {IconView("NSTouchBarTextLeftAlignTemplate")})
-                
-                Button(action: {
-                    self.updateAlignment(to: .center)
-                }, label: {IconView("NSTouchBarTextCenterAlignTemplate")})
-                
-                Button(action: {
-                    self.updateAlignment(to: .trailing)
-                }, label: {IconView("NSTouchBarTextRightAlignTemplate")})
-                
-                Button(action: {
-                    self.updateSize(by: -10)
-                }, label: {IconView("NSTouchBarGoDownTemplate")})
-                
-                Button(action: {
-                    self.updateSize(by: 10)
-                }, label: {IconView("NSTouchBarGoUpTemplate")})
-                
-                IconView("NSFontPanel")
-                ColorWellView()
+                // Bold, Italic, Underline
+                Group {
+                    IconView("NSTouchBarTextBoldTemplate")
+                    IconView("NSTouchBarTextItalicTemplate")
+                    IconView("NSTouchBarTextUnderlineTemplate")
+                }
+                // Alignment
+                Group {
+                    Button(action: {
+                        self.updateAlignment(to: .leading)
+                    }, label: {IconView("NSTouchBarTextLeftAlignTemplate")})
+                    
+                    Button(action: {
+                        self.updateAlignment(to: .center)
+                    }, label: {IconView("NSTouchBarTextCenterAlignTemplate")})
+                    
+                    Button(action: {
+                        self.updateAlignment(to: .trailing)
+                    }, label: {IconView("NSTouchBarTextRightAlignTemplate")})
+                }
+                // Size, Font, Color
+                Group {
+                    Button(action: {
+                        self.updateSize(by: -10)
+                    }, label: {IconView("NSTouchBarGoDownTemplate")})
+                    
+                    Button(action: {
+                        self.updateSize(by: 10)
+                    }, label: {IconView("NSTouchBarGoUpTemplate")})
+                    
+                    IconView("NSFontPanel")
+                    ColorWellView()
+                }
             }
+            .buttonStyle(buttonStyle)
             
         }
         .frame(width: 400, height: 35)
